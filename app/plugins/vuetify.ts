39de0066231ef55334b41017c3a0ac4bbf89a885
christian arnidoval <chanooh14@gmail.com>
2026-09-07 10:19:33 +0800

    new push

--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -1,12 +1,8 @@
-// import this after install `@mdi/font` package
-import '@mdi/font/css/materialdesignicons.css'
-
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-
-export default defineNuxtPlugin((app) => {
-  const vuetify = createVuetify({
-    // ... your configuration
-  })
-  app.vueApp.use(vuetify)
+//@ts-nocheck
+export default defineNuxtConfig({
+ runtimeConfig: {
+ public: {
+ googleClientId: process.env.NUXT_PUBLIC_GOOGLE_CLIENT_ID
+ }
+ }
 })
